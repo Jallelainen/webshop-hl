6eb15737d42327259f49e60cfb4b7a049c085fa5
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2022-03-10 15:02:20 +0100

    fix href warning

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,14 +5,14 @@ export const Header = () => {
        <div className="row">
            <nav class="navbar navbar-expand-lg navbar-light">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">SPORTSHOES.COM</a>
+                    <a class="navbar-brand" href="http://localhost:3000">SPORTSHOES.COM</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#">MEN</a>
-                            <a class="nav-link" href="#">WOMEN</a>
+                            <a class="nav-link active" aria-current="page" href="http://localhost:3000">MEN</a>
+                            <a class="nav-link" href="http://localhost:3000">WOMEN</a>
                         </div>
                     </div>
                 </div>
