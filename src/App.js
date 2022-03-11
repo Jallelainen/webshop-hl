78fc65ff9d18f8369760bbe995f6be7eefdc61bf
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2022-03-11 12:14:54 +0100

    bugfix with img

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import {Header} from './Components/Header'
 import {Carousel} from './Components/Carousel'
 
 function App() {
+
   return (
     <div className="container-fluid">
       <Header/>
       <Carousel/>
       <div className="row">
-
+   
       </div>
       <div className="row">
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export const Header = () => {
     return (
        <div className="row">
-           <nav class="navbar navbar-expand-lg navbar-light">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="http://localhost:3000">SPORTSHOES.COM</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+           <nav className="navbar navbar-expand-lg navbar-light header-nav">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="http://localhost:3000">SPORTSHOES.COM</a>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="http://localhost:3000">MEN</a>
-                            <a class="nav-link" href="http://localhost:3000">WOMEN</a>
+                    <div className="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            <a className="nav-link active" aria-current="page" href="http://localhost:3000">MEN</a>
+                            <a className="nav-link" href="http://localhost:3000">WOMEN</a>
                         </div>
                     </div>
                 </div>
