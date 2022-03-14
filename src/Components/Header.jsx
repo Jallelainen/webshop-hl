6b6390dadc74057cf57407d938a1ba52401eff29
Marcus Jarlevid <70881791+Jallelainen@users.noreply.github.com>
2022-03-14 10:06:11 +0100

    fix header fixed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 export const Header = () => {
     return (
        <div className="row">
-           <nav className="navbar navbar-expand-lg navbar-light header-nav">
+           <nav className="navbar fixed-top navbar-expand-lg navbar-light header-nav">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="http://localhost:3000">SPORTSHOES.COM</a>
+                    <a className="navbar-brand text-light h1" href="http://localhost:3000">SPORTSHOES.COM</a>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNavAltMarkup">
                         <div className="navbar-nav">
-                            <a className="nav-link active" aria-current="page" href="http://localhost:3000">MEN</a>
-                            <a className="nav-link" href="http://localhost:3000">WOMEN</a>
+                            <a className="nav-link active text-light" aria-current="page" href="http://localhost:3000">MEN</a>
+                            <a className="nav-link text-light" href="http://localhost:3000">WOMEN</a>
                         </div>
                     </div>
                 </div>
